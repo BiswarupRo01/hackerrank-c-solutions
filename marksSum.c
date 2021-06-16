9074a28caad5e10ad63cc01476f9c16a9119b611
Biswarup Roy <76861599+BiswarupRo01@users.noreply.github.com>
2021-06-16 13:37:40 +0530

    Create marksSum.c

---
+++ marksSum.c
@@ -0,0 +1,39 @@
+// Check the problem statement at : https://www.hackerrank.com/challenges/students-marks-sum/problem
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+//Complete the following function.
+
+int marks_summation(int* marks, int number_of_students, char gender) {
+  //Write your code here.
+  int start;
+  int sum = 0;
+  if(gender == 'b') start = 0;
+  else if(gender == 'g') start = 1;
+  for(;start<number_of_students;start+=2){
+      sum += marks[start];
+  }
+  return sum;
+}
+
+int main() {
+    int number_of_students;
+    char gender;
+    int sum;
+  
+    scanf("%d", &number_of_students);
+    int *marks = (int *) malloc(number_of_students * sizeof (int));
+ 
+    for (int student = 0; student < number_of_students; student++) {
+        scanf("%d", (marks + student));
+    }
+    
+    scanf(" %c", &gender);
+    sum = marks_summation(marks, number_of_students, gender);
+    printf("%d", sum);
+    free(marks);
+ 
+    return 0;
+}
