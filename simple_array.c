7a02e591d83141394499e99dd7ecd7da983c1f2c
Biswarup Roy <76861599+BiswarupRo01@users.noreply.github.com>
2021-06-20 17:07:19 +0530

    Create simple_array.c

---
+++ simple_array.c
@@ -0,0 +1,23 @@
+// Check the problem statement here : https://www.hackerrank.com/challenges/1d-arrays-in-c/problem
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int n;
+    scanf("%d",&n);
+    int *arr = (int*)malloc(n*sizeof(int));
+    int sum = 0;   
+    for(int i=0; i<n; i++){
+        scanf("%d",&arr[i]);
+        sum += arr[i];
+    }
+    
+    printf("%d \n",sum);
+    
+    return 0;
+}
